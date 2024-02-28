4711de0a747afacc4ed3ef27a9b684f21caa0993
KrithiK1107* <shruthihashini@gmail.com>
2024-02-28 14:25:02 +0530

    new files variables and outputs

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
 
 
-variable  "example" {
-  type        = "example"
-  location =  "westus2"
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
 }
 
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = random_pet.rg_name.id
+}
